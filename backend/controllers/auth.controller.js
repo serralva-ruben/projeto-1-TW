5f05c096557733da8d43b1bb2a8008c7c6e6a7e4
Gheorghiu Alin <aggheorghiu@hotmail.com>
2023-06-20 01:30:56 +0300

    register login working

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,54 +1,57 @@
 const User = require('../models/user.model');
-const bcrypt =  require("bcrypt");
-const jwt =  require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
 
 const register = async (req, res, next) => {
-  bcrypt.hash(req.body.password, 10, function(err, hashedPass){
-    if(err){
-      res.json({
-        error: err
-      })
-    }
-    let user = new User({
-      email: req.body.email,
-      password: hashedPass,
-    })
-    user.save().then(user=>{
-      res.json({
-        message: 'User added successfully!'
-      })
-    })
-    .catch(error => {
-      res.json({
-        message: 'An error occured!',
-      })
-    })
-  })
+  try {
+    bcrypt.hash(req.body.password, 10, function (err, hashedPass) {
+      if (err) {
+        res.json({
+          error: err
+        });
+      }
+      let user = new User({
+        username: req.body.username,
+        password: hashedPass,
+      });
+      user.save()
+        .then(user => {
+          res.json({
+            message: 'User added successfully!'
+          });
+        })
+    });
+  } catch (error) {
+    res.json({
+      message: 'An error occurred!'
+    });
+  }
 };
 
+
 const login = async (req, res, next) => {
-  let email = req.body.email;
+  let username = req.body.username;
   let password = req.body.password;
 
-  User.findOne({$or: [{email:email},{name:email}]}).then(user => {
+  User.findOne({ $or: [{ username: username }, { name: username }] }).then(user => {
     console.log(user)
-    if(user){
-      bcrypt.compare(password, user.password, function(err, result){
-        if(err) {res.json({error: err})}
-        if(result){
-          let token = jwt.sign({name: user.name}, 'private key :) I hope no one knows me')
+    if (user) {
+      bcrypt.compare(password, user.password, function (err, result) {
+        if (err) { res.json({ error: err }) }
+        if (result) {
+          let token = jwt.sign({ name: user.name }, 'private key :) I hope no one knows me')
           res.json({
             message: 'Login Successful!',
             token: token,
           })
         }
-        else{res.json({message: 'Password not matched!'})}
+        else { res.json({ message: 'Password not matched!' }) }
       })
-    }else{res.json({message: 'No user found!'})}
+    } else { res.json({ message: 'No user found!' }) }
   })
 };
 
--- frontend_react/src/components/QuizComponent.js
+++ frontend_react/src/components/QuizComponent.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import QuestionComponent from './QuestionComponent';
 import Navbar from './Navbar';
 
