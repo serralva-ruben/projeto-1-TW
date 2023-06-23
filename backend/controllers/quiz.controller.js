d3c7dc149e82cb2d3bf1a9ff41551798cae7e483
serralva-ruben <91135825+serralva-ruben@users.noreply.github.com>
2023-06-23 14:25:56 +0000

    removed useless functions

--- backend/controllers/quiz.controller.js
+++ backend/controllers/quiz.controller.js
@@ -16,61 +16,8 @@ const getQuiz = async (req, res, next) => {
   res.status(200).send(quiz);
 };
 
-const evaluateQuiz = async (req, res) => {
-  try {
-    const submittedAnswers = req.body; 
-    const quizTitle = req.params.quizTitle; 
-    const quizSolutions = await quizzessolutions.findOne({ title: quizTitle });
-    const score = calculateScore(submittedAnswers, quizSolutions.solutions);
-
-    res.json({ score });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to evaluate quiz answers' });
-  }
-};
-
-
-const calculateScore = (submittedAnswers, correctAnswers) => {
-  let score = 0;
-
-  for (let i = 0; i < submittedAnswers.length; i++) {
-    const submittedAnswer = submittedAnswers[i];
-    const correctAnswer = correctAnswers[i];
-
-    if (Array.isArray(submittedAnswer)) {
-      if (arraysMatch(submittedAnswer, correctAnswer)) {
-        score += 1;
-      }
-    } else {
-      if (submittedAnswer === correctAnswer) {
-        score += 1;
-      }
-    }
-  }
-
-  return score;
-};
-
-
-const arraysMatch = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-
 
 module.exports = {
   getQuizzes,
   getQuiz,
-  evaluateQuiz,
 }
