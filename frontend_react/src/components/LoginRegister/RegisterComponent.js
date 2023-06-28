a9804a81fc3be060f157831435d80f70ea1dd9e4
Alamaos <alamaosoficial@gmail.com>
2023-06-28 14:11:04 +0000

    Minor Debug/Clean

--- frontend_react/src/components/LoginRegister/RegisterComponent.js
+++ frontend_react/src/components/LoginRegister/RegisterComponent.js
@@ -1,5 +1,4 @@
-import { Link } from 'react-router-dom';
-import { useNavigate } from "react-router-dom";
+import {Link, useNavigate} from 'react-router-dom';
 import '../../style/Register.css'
 
 function RegisterComponent() {
